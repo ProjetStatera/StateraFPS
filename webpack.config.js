ab8a7901eb078127899f931e5253801d45e46902
AdemBenJabria <71251810+AdemBenJabria@users.noreply.github.com>
2022-03-08 19:54:02 +0100

    try to deploy it

--- webpack.config.js
+++ webpack.config.js
@@ -13,14 +13,14 @@ module.exports = {
         extensions: [".tsx", ".ts", ".js"],
     },
     devServer: {
-        host: "0.0.0.0",
-        port: 8080, //port that we're using for local host (localhost:8080)
-        static: path.resolve(appDirectory, "public"), //tells webpack to serve from the public folder
-        hot: true,
-        devMiddleware: {
-            publicPath: "/",
-        }
-    },
+    host: "0.0.0.0",
+    port: 8080, //port that we're using for local host (localhost:8080)
+    static: path.resolve(appDirectory, "public"), //tells webpack to serve from the public folder
+    hot: true,
+    devMiddleware: {
+        publicPath: "/",
+    }
+},
     module: {
         rules: [
             {
