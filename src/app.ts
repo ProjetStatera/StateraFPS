40e289268eb1acbbfa9cfdfcdac206e6283f1303
AdemBenJabria <71251810+AdemBenJabria@users.noreply.github.com>
2022-03-09 06:19:49 +0100

    zombie's basic animations done

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ import { SkyMaterial } from "@babylonjs/materials";
 import { AdvancedDynamicTexture, StackPanel, Button, TextBlock, Rectangle, Control, Image } from "@babylonjs/gui";
 import { FirstPersonController } from "./FirstPersonController";
 import { Enemy } from "./Enemy";
-import { Engine, int, Tools, ArcRotateCamera, OimoJSPlugin, SpotLight, HemisphericLight, Scene, Animation, Vector3, Mesh, Color3, Color4, ShadowGenerator, GlowLayer, PointLight, FreeCamera, CubeTexture, Sound, PostProcess, Effect, SceneLoader, Matrix, MeshBuilder, Quaternion, AssetsManager, StandardMaterial, PBRMaterial, Material } from "@babylonjs/core";
+import { Engine, int, KeyboardEventTypes, Tools, ArcRotateCamera, OimoJSPlugin, SpotLight, HemisphericLight, Scene, Animation, Vector3, Mesh, Color3, Color4, ShadowGenerator, GlowLayer, PointLight, FreeCamera, CubeTexture, Sound, PostProcess, Effect, SceneLoader, Matrix, MeshBuilder, Quaternion, AssetsManager, StandardMaterial, PBRMaterial, Material, float, Light } from "@babylonjs/core";
 
 enum State { START = 0, GAME = 1, LOSE = 2, CUTSCENE = 3 }
 
@@ -19,6 +19,10 @@ class App {
     private fps: FirstPersonController;
     private zombie: Enemy;
     private difficulty: int;
+    private velocity: float;
+    private _transition: boolean = false;
+    private light1: Light;
+    private skyboxMaterial: SkyMaterial;
 
     private _gameScene: Scene;
 
@@ -123,6 +127,7 @@ class App {
             medium.width = "5%";
             hard.width = "5%";
             this.difficulty = 400;
+            this.velocity = 0.1;
         });
 
         //medium
@@ -138,6 +143,7 @@ class App {
             medium.width = "8%";
             hard.width = "5%";
             this.difficulty = 250;
+            this.velocity = 0.3;
         });
 
         //hard
@@ -152,6 +158,7 @@ class App {
             medium.width = "5%";
             hard.width = "8%";
             this.difficulty = 100;
+            this.velocity = 0.5;
         });
 
 
@@ -171,8 +178,9 @@ class App {
 
     private async CreateMap(): Promise<void> {
         var light1: HemisphericLight = new HemisphericLight("light1", new Vector3(0, 1, 0), this._scene); //white light
-        light1.intensity = 1;
+        light1.intensity = 0.05;
         light1.range = 100;
+        this.light1 = light1;
 
         // Sky material
         var skyboxMaterial = new SkyMaterial("skyMaterial", this._scene);
@@ -181,10 +189,11 @@ class App {
         // Sky mesh (box)
         var skybox = Mesh.CreateBox("skyBox", 1000.0, this._scene);
         skybox.material = skyboxMaterial;
-        skyboxMaterial.luminance = 1;
+        skyboxMaterial.luminance = 0;
+        this.skyboxMaterial = skyboxMaterial;
 
         // Manually set the sun position
-        skyboxMaterial.useSunPosition = true; // Do not set sun position from azimuth and inclination
+        skyboxMaterial.useSunPosition = false; // Do not set sun position from azimuth and inclination
         skyboxMaterial.sunPosition = new Vector3(0, 100, 0);
 
         const result = await SceneLoader.ImportMeshAsync("", "./models/", "SampleScene.glb", this._scene);
@@ -200,6 +209,31 @@ class App {
         })
     }
 
+    private KeyboardInput(): void {
+        this._scene.onKeyboardObservable.add((kbInfo) => {
+            switch (kbInfo.type) {
+                case KeyboardEventTypes.KEYDOWN:
+                    switch (kbInfo.event.key) {
+                        case 'n':
+                            if (this.light1.intensity != 1) {
+                                this.skyboxMaterial.luminance = 1;
+                                this.light1.intensity = 1;
+                                this.skyboxMaterial.useSunPosition = true; // Do not set sun position from azimuth and inclination
+                                this.skyboxMaterial.sunPosition = new Vector3(0, 100, 0);
+                            } else {
+                                this.skyboxMaterial.luminance = 0;
+                                this.light1.intensity = 0.05;
+                                this.skyboxMaterial.useSunPosition = false;
+                            }
+                            break;
+                    }
+                    break;
+            }
+        })
+    }
+
+
+
     /**
      * launch FirstPersonController.ts
      */
@@ -208,7 +242,7 @@ class App {
         this._gameScene = scene;
         this._scene.detachControl();
         this.fps = new FirstPersonController(this._gameScene, this._canvas);
-        this.zombie = new Enemy(this._gameScene, this._canvas, this.difficulty);
+        this.zombie = new Enemy(this._gameScene, this._canvas, this.difficulty, this.velocity);
         this._gameScene.onPointerDown = (evt) => {
             if (evt.button === 0)//left click
             {
@@ -233,6 +267,93 @@ class App {
         await this._scene.whenReadyAsync();
         this._engine.hideLoadingUI();
         this._scene.debugLayer.show();
+    }
+
+    public async goToLose(): Promise<void> {
+        this._engine.displayLoadingUI();
+
+        //--SCENE SETUP--
+        this._scene.detachControl();
+        let scene = new Scene(this._engine);
+        scene.clearColor = new Color4(0, 0, 0, 1);
+        let camera = new FreeCamera("camera1", new Vector3(0, 0, 0), scene);
+        camera.setTarget(Vector3.Zero());
+
+        //--GUI--
+        const guiMenu = AdvancedDynamicTexture.CreateFullscreenUI("UI");
+        guiMenu.idealHeight = 720;
+
+        //background image
+        const image = new Image("lose", "sprites/lose.jpeg");
+        image.autoScale = true;
+        guiMenu.addControl(image);
+
+        const panel = new StackPanel();
+        guiMenu.addControl(panel);
+
+        const text = new TextBlock();
+        text.fontSize = 24;
+        text.color = "white";
+        text.height = "100px";
+        text.width = "100%";
+        panel.addControl(text);
+
+        text.textHorizontalAlignment = TextBlock.HORIZONTAL_ALIGNMENT_CENTER;
+        text.textVerticalAlignment = TextBlock.VERTICAL_ALIGNMENT_CENTER;
+        text.text = "You died !";
+        const dots = new TextBlock();
+        dots.color = "white";
+        dots.fontSize = 24;
+        dots.height = "100px";
+        dots.width = "100%";
+        dots.text = "...."
+
+        const mainBtn = Button.CreateSimpleButton("mainmenu", "MAIN MENU");
+        mainBtn.width = 0.2;
+        mainBtn.height = "40px";
+        mainBtn.color = "white";
+        panel.addControl(mainBtn);
+
+        Effect.RegisterShader("fade",
+            "precision highp float;" +
+            "varying vec2 vUV;" +
+            "uniform sampler2D textureSampler; " +
+            "uniform float fadeLevel; " +
+            "void main(void){" +
+            "vec4 baseColor = texture2D(textureSampler, vUV) * fadeLevel;" +
+            "baseColor.a = 1.0;" +
+            "gl_FragColor = baseColor;" +
+            "}");
+
+        let fadeLevel = 1.0;
+        this._transition = false;
+        scene.registerBeforeRender(() => {
+            if (this._transition) {
+                fadeLevel -= .05;
+                if (fadeLevel <= 0) {
+                    // this._goToCutScene();
+                    this.goToStart();
+                    this._transition = false;
+                }
+            }
+        })
+
+        //this handles interactions with the start button attached to the scene
+        mainBtn.onPointerUpObservable.add(() => {
+            //todo: add fade transition & selection sfx
+            scene.detachControl();
+            guiMenu.dispose();
+            // this._goToStart();
+            this._transition = true;
+        });
+
+        //--SCENE FINISHED LOADING--
+        await scene.whenReadyAsync();
+        this._engine.hideLoadingUI(); //when the scene is ready, hide loading
+        //lastly set the current state to the lose state and set the scene to the lose scene
+        this._scene.dispose();
+        this._scene = scene;
+        this._state = State.LOSE;
     }
 
     /**
--- src/enemy.ts
+++ src/enemy.ts
@@ -1,13 +1,14 @@
 import * as BabylonViewer from '@babylonjs/viewer';
-import { Engine, Tools, Space, AnimationGroup, int, AbstractMesh, float, ArcRotateCamera, OimoJSPlugin, SpotLight, HemisphericLight, Scene, Animation, Vector3, Mesh, Color3, Color4, ShadowGenerator, GlowLayer, PointLight, FreeCamera, CubeTexture, Sound, PostProcess, Effect, SceneLoader, Matrix, MeshBuilder, Quaternion, AssetsManager, StandardMaterial, PBRMaterial, Material } from "@babylonjs/core";
+import { Engine, Tools, KeyboardEventTypes, Space, AnimationGroup, int, AbstractMesh, float, ArcRotateCamera, OimoJSPlugin, SpotLight, HemisphericLight, Scene, Animation, Vector3, Mesh, Color3, Color4, ShadowGenerator, GlowLayer, PointLight, FreeCamera, CubeTexture, Sound, PostProcess, Effect, SceneLoader, Matrix, MeshBuilder, Quaternion, AssetsManager, StandardMaterial, PBRMaterial, Material } from "@babylonjs/core";
 import { FirstPersonController } from "./FirstPersonController";
 import { Player } from "./Player";
 
 export class Enemy {
-    public currentEnemyHealth: float;
+    private currentEnemyHealth: float;
     private damage: float;
     private playerDamage: float;
     private time: float;
+    private velocity: float;
 
     public camera: FreeCamera;
     public scene: Scene;
@@ -28,10 +29,12 @@ export class Enemy {
     private _walk: AnimationGroup;
     private _walk2: AnimationGroup;
 
-    constructor(scene: Scene, canvas: HTMLCanvasElement, difficulty: int) {
+    constructor(scene: Scene, canvas: HTMLCanvasElement, difficulty, velocity: int) {
         this.scene = scene;
         this._canvas = canvas;
+        this.velocity = velocity;
         this.spawner(difficulty);
+        this.KeyboardInput();
     }
 
     /*private async spawner(difficulty: int): Promise<any> {
@@ -44,9 +47,7 @@ export class Enemy {
         }
     }*/
     private async spawner(difficulty: int): Promise<any> {
-        for (let i = 0; i <= 20; i++) {
-            this.CreateEnemy(new Vector3(this.getRandomInt(difficulty),0,this.getRandomInt(difficulty)));
-        }
+        this.CreateEnemy(new Vector3(this.getRandomInt(difficulty), 0, this.getRandomInt(difficulty)));
     }
 
 
@@ -56,11 +57,10 @@ export class Enemy {
         let env = result.meshes[0];
         let allMeshes = env.getChildMeshes();
         env.position = position;
-        env.scaling = new Vector3(0.02, 0.02, -0.02);
+        env.scaling = new Vector3(0.02, 0.02, 0.02);
         env.name = "zombie";
         this.zombie = env;
-        this._setUpAnimations();
-        //this.scene.onBeforeRenderObservable.add(function () { this.chase() });
+
 
         this._attack = this.scene.getAnimationGroupByName("Zombie@Z_Attack");
         this._fallingBack = this.scene.getAnimationGroupByName("Zombie@Z_FallingBack");
@@ -69,10 +69,51 @@ export class Enemy {
         this._run = this.scene.getAnimationGroupByName("Zombie@Z_Run_InPlace");
         this._walk = this.scene.getAnimationGroupByName("Zombie@Z_Walk1_InPlace");
         this._walk2 = this.scene.getAnimationGroupByName("Zombie@Z_Walk_InPlace");
+        this._setUpAnimations();
+        this._animatePlayer()
 
     }
 
-    private chase() {
+    private KeyboardInput(): void {
+        this.scene.onKeyboardObservable.add((kbInfo) => {
+            switch (kbInfo.type) {
+                case KeyboardEventTypes.KEYDOWN:
+                    switch (kbInfo.event.key) {
+                        case 'z':
+                        case 's':
+                        case 'q':
+                        case 'd':
+                            this.chase(this.velocity*1.1);
+                            break;
+                        case 'Shift':
+                            this.chase(this.velocity*2);
+                            break;
+                        case 'Control':
+                            this.chase(this.velocity*3);
+                            break;
+                    }
+                    break;
+            }
+        })
+        this.scene.onKeyboardObservable.add((kbInfo) => {
+            switch (kbInfo.type) {
+                case KeyboardEventTypes.KEYUP:
+                    switch (kbInfo.event.key) {
+                        case 'z':
+                        case 's':
+                        case 'q':
+                        case 'd':
+                        this.chase(this.velocity);
+                        this._currentAnim = this._idle;
+                    }
+                    break;
+            }
+        })
+    }
+
+
+
+    private chase(velocity: float) {
         let zombie = this.zombie;
         let scene = this.scene;
         let camera = scene.getCameraByName("camera");
@@ -84,25 +125,43 @@ export class Enemy {
             let targetVec = camera.position.subtract(initVec);
             let targetVecNorm = Vector3.Normalize(targetVec);
 
-            // Move enemy towards the player and stops slightly ahead
-            if (distVec < 40) {
-                distVec -= 0.05;
-                zombie.translate(targetVecNorm, 0.05, Space.WORLD);
+            if(distVec < 6)
+            {
                 this.attack();
-
             }
+            // Move enemy towards the player and stops slightly ahead
+            if (velocity >= 0.5) {
+                this._currentAnim = this._run;
+                this._animatePlayer();
+            }
+            else if(velocity == 0){
+                this._currentAnim = this._idle;
+                this._animatePlayer();
+            }
+            else if (velocity > 0 && velocity < 0.5)  {
+                
+                this._currentAnim = this._walk;
+                this._animatePlayer();
+            }
+            distVec -= velocity;
+            zombie.translate(new Vector3(targetVecNorm._x, 0, targetVecNorm._z,), velocity, Space.WORLD);
 
             // Enemy always faces the player
             zombie.setParent(null);
-            zombie.lookAt(camera.position, Math.PI, Math.PI / 2);
-
+            zombie.lookAt(camera.position);
         }
     }
 
     private attack() {
-        this._currentAnim = this._attack;
-        this._animatePlayer()
+        if(!this._attack.isPlaying)
+        this._attack.play();
+        else {
+            this.velocity = 0;
+            this._attack.play();
+        }
+
     }
+
 
     private _animatePlayer(): void {
         if (this._currentAnim != null && this._prevAnim !== this._currentAnim) {
@@ -114,8 +173,14 @@ export class Enemy {
 
     private _setUpAnimations(): void {
         //initialize current and previous
-        this._currentAnim = this._walk;
-        this._prevAnim = this._walk2;
+        this._currentAnim = this._idle;
+        this._prevAnim = this._fallingBack;
+        this._run.loopAnimation = true;
+        this._idle.loopAnimation = true;
+        this._walk.loopAnimation = true;
+        this._walk2.loopAnimation = true;
+        this._attack.loopAnimation = true;
+        this._walk2.speedRatio = 2;
     }
 
     private getRandomInt(max) {
