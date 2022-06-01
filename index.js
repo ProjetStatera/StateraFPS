4f3bd34b895f45c2d8f3583f057777f87e7daf3b
AdemBenJabria <71251810+AdemBenJabria@users.noreply.github.com>
2022-06-01 17:44:39 +0200

    make it better

---
+++ index.js
@@ -0,0 +1,15 @@
+let express = require('express');
+let app = express();
+
+app.use("/", express.static(__dirname));
+
+app.get("/", function(req, res) {
+    res.sendFile("index.html");
+});
+
+// necessary for heroku, as heroku will position the PORT environment variable
+let port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log("Server is running on port " + port);
+});
