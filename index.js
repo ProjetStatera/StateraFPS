7a306ffd4df082b8635b12e31490160003843a1a
AdemBenJabria <71251810+AdemBenJabria@users.noreply.github.com>
2022-06-01 19:08:08 +0200

    new one

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let express = require('express');
+/*let express = require('express');
 let app = express();
 
 app.use("/", express.static(__dirname));
@@ -12,4 +12,36 @@ let port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log("Server is running on port " + port);
+});*/
+let express = require('express');
+let app = express();
+let http = require('http').Server(app);
+let io = require('socket.io')(http);
+let path = require ('path');
+let os = require('os');
+let ifaces = os.networkInterfaces();
+let port = process.env.PORT || 8000; // connexion heroku
+
+// ================================================
+// SHOW IP ADDRESS IN CONSOLE
+console.log('=============');
+console.log('IP ADDRESS:');
+Object.keys(ifaces).forEach(function (ifname) {
+    var alias = 0;
+
+    ifaces[ifname].forEach(function (iface) {
+    if ('IPv4' !== iface.family || iface.internal !== false) {
+        // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+        return;
+    }
+    if (alias >= 1) {
+        // this single interface has multiple ipv4 addresses
+        console.log(ifname + ':' + alias, iface.address);
+    } else {
+        // this interface has only one ipv4 adress
+        console.log(ifname, iface.address);
+    }
+    ++alias;
+    });
 });
+console.log('=============');
