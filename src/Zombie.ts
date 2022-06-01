2d16cb6f03698fbbb8c137787150984651f0502b
AdemBenJabria <71251810+AdemBenJabria@users.noreply.github.com>
2022-06-01 23:50:37 +0200

    Update Zombie.ts

--- src/Zombie.ts
+++ src/Zombie.ts
@@ -6,7 +6,7 @@ import { Enemy } from "./Enemy";
 export class Zombie extends Enemy {
 
     public override async CreateEnemy(position: Vector3): Promise<any> {
-    const result = await SceneLoader.ImportMeshAsync("zombie", "/models", "zombie.babylon", this.scene);
+    const result = await SceneLoader.ImportMeshAsync("zombie", "/models", "boss.glb", this.scene);
     let env = result.meshes[0];
     let allMeshes = env.getChildMeshes();
     env.position = position;
